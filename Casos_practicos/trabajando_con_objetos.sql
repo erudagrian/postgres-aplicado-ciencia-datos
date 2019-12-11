f320dcf1303d8217fb08fed2fd24831ca75d446a
Israel Baurel Vázquez Morales <israelvazquezmorales@gmail.com>
2019-12-11 02:29:39 -0600

    trabajando_con_objetos

--- Casos_practicos/trabajando_con_objetos.sql
+++ Casos_practicos/trabajando_con_objetos.sql
@@ -26,29 +26,3 @@ FROM
    ordenes
 WHERE
    info -> 'items' ->> 'producto' = 'Biberón'
-
---------------------- Agregando objetos
-
-SELECT
-   MIN (
-      CAST (
-         info -> 'items' ->> 'cantidad' AS INTEGER
-      )
-   ),
-   MAX (
-      CAST (
-         info -> 'items' ->> 'cantidad' AS INTEGER
-      )
-   ),
-   SUM (
-      CAST (
-         info -> 'items' ->> 'cantidad' AS INTEGER
-      )
-   ),
-   AVG (
-      CAST (
-         info -> 'items' ->> 'cantidad' AS INTEGER
-      )
-   )
-FROM
-   ordenes;
